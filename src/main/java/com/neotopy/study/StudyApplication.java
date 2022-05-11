40cde6ee66d74c855dc4229c2ff00292eb148e39
yuchacss <cktptls@gmail.com>
2022-05-12 00:00:42 +0900

    first init

---
+++ src/main/java/com/neotopy/study/StudyApplication.java
@@ -0,0 +1,16 @@
+package com.neotopy.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApplication.class, args);
+	}
+
+}
